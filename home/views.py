450426a9defe052905fa2b266c9f3ac5d96923f7
MWasile <maaciej.wasilewski@gmail.com>
2022-10-28 20:34:55 +0200

    create app for homepage / main site form

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = 'home/landingpage.html'
+
+
+class AddDonationView(TemplateView):
+    template_name = 'home/form.html'
