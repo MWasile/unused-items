4a1f42bada76e27dd7bd502ae09aeef980ee0b9c
MWasile <maaciej.wasilewski@gmail.com>
2022-11-03 11:04:32 +0100

    Remove autologin, modify the success url

--- user/views.py
+++ user/views.py
@@ -12,9 +12,8 @@ class LoginUserView(TemplateView):
 class RegiserUserView(FormView):
     template_name = 'user/register.html'
     form_class = forms.ReigsterUserForm
-    success_url = reverse_lazy('home:landing_page')
+    success_url = reverse_lazy('user:login')
 
     def form_valid(self, form):
         new_user = form.create_new_user()
-        login(self.request, new_user)
         return super().form_valid(form)
