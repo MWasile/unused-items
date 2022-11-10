be968affccc0b7b6c34806b922c4c8b5eef6bb57
MWasile <maaciej.wasilewski@gmail.com>
2022-11-10 23:30:30 +0100

    add pural name for django admin panel

--- home/models.py
+++ home/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=150)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return f'Category: {self.name}'
 
